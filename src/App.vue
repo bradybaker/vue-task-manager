f770af17b635d4db81b70a5cfc492dfa1787b91b
Brady Baker <bradybaker82@gmail.com>
2021-03-30 10:16:19 -0500

    Task reminder is now togglable

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="container">
   <Header title="Task Tracker" />
-  <Tasks @delete-task="deleteTask" :tasks="tasks"/>
+  <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks"/>
 </div>
 </template>
 
@@ -25,7 +25,10 @@ export default {
       if (confirm('Are you sure?')) {
         this.tasks = this.tasks.filter((task) => task.id !== id) 
       }
-    } 
+    },
+    toggleReminder(reminder, id) {
+      this.tasks[id - 1].reminder = !reminder
+    }
   },
   created() {
     this.tasks = [
