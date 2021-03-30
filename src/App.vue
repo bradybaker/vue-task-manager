c99c4c62038efd7607607fb0f4589287ed1289f9
Brady Baker <bradybaker82@gmail.com>
2021-03-30 10:44:46 -0500

    Built form for new task, working on emit next

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="container">
   <Header title="Task Tracker" />
+  <AddTask />
   <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks"/>
 </div>
 </template>
@@ -8,12 +9,14 @@
 <script>
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 export default {
   name: 'App',
   components: {
     Header,
     Tasks,
+    AddTask
   },
   data() {
     return {
@@ -27,7 +30,8 @@ export default {
       }
     },
     toggleReminder(reminder, id) {
-      this.tasks[id - 1].reminder = !reminder
+      // this.tasks[id - 1].reminder = !reminder
+      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task )
     }
   },
   created() {
---
+++ src/components/AddTask.vue
@@ -0,0 +1,90 @@
+<template>
+    <form @submit="onSubmit" class="add-form">
+        <div class="form-control">
+            <label>Task</label>
+            <input type="text" v-model="task" name="task" placeholder="Add Task"/>
+        </div>
+        <div class="form-control">
+            <label>Day & Time</label>
+            <input type="text" v-model="day" name="day" placeholder="Add Day & Time"/>
+        </div>
+        <div class="form-control form-control-check">
+            <label>Set Reminder</label>
+            <input type="checkbox" name="reminder" v-model="reminder"/>
+        </div>
+        <input type="submit" value="Save Task" class="btn btn-block" />
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'AddTask',
+    data() {
+        return {
+            task: '',
+            day: '',
+            reminder: false
+        }
+    },
+    methods: {
+        onSubmit(e) {
+            e.preventDefault()
+
+            if(!this.task) {
+                alert('Please add a task')
+                return
+            }
+            const newTask = {
+							id: Math.floor(Math.random() * 100000),
+							task: this.task,
+							day: this.day,
+							reminder: this.reminder
+            }
+
+						console.log('This is the new task', newTask)
+
+						this.task = '',
+						this.day = '',
+						this.reminder = false
+        }
+    }
+}
+</script>
+
+<style scoped>
+.add-form {
+    margin-bottom: 40px;
+}
+
+.form-control {
+    margin: 20px 0;
+}
+
+.form-control label {
+    display: block;
+}
+
+.form-control input {
+    width: 100%;
+    height: 40px;
+    margin: 5px;
+    padding: 3px 7px;
+    font-size: 17px;
+}
+
+.form-control-check {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.form-control-check label {
+    flex: 1;
+}
+
+.form-control-check input {
+    flex: 2;
+    height: 20px;
+}
+
+</style>
