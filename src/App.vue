65df86af4fea91e8ff8c1f8df2c3ff8e9b9b17f4
Brady Baker <bradybaker82@gmail.com>
2021-03-30 11:13:42 -0500

    Can now toggle the form component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
 <div class="container">
-  <Header title="Task Tracker" />
-  <AddTask @add-task="addTask" />
+  <Header @toggle-add-task="toggleAddTask" title="Task Tracker" />
+  <div v-show="showAddTask">
+    <AddTask @add-task="addTask" />
+  </div>
   <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks"/>
 </div>
 </template>
@@ -20,7 +22,8 @@ export default {
   },
   data() {
     return {
-      tasks: []
+      tasks: [],
+      showAddTask: false
     }
   },
   methods: {
@@ -35,6 +38,9 @@ export default {
     },
     addTask(newTask) {
       this.tasks = [...this.tasks, newTask]
+    },
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask
     }
   },
   created() {
