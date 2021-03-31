566cdec4df7a1e760389da897b336ad4c332989e
Brady Baker <bradybaker82@gmail.com>
2021-03-31 12:21:02 -0500

    removed task array from front end

--- src/App.vue
+++ src/App.vue
@@ -41,29 +41,15 @@ export default {
     },
     toggleAddTask() {
       this.showAddTask = !this.showAddTask
+    },
+    async fetchTasks() {
+      const res = await fetch('http://localhost:5000/tasks')
+      const data = await res.json()
+      return data;
     }
   },
-  created() {
-    this.tasks = [
-      {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'March 1st at 2:30pm',
-        reminder: true
-      },
-      {
-        id: 2,
-        text: 'Meeting at school',
-        day: 'March 3rd at 1:30pm',
-        reminder: true
-      },
-      {
-        id: 3,
-        text: 'Food shopping',
-        day: 'March 3rd at 11:00am',
-        reminder: false
-      },
-    ]
+  async created() {
+    this.tasks = await this.fetchTasks();
   }
 }
 </script>
