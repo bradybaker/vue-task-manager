75639667fefe00b80d8fccf4d1ab1df70d11c2fd
Brady Baker <bradybaker82@gmail.com>
2021-04-01 16:15:09 -0500

    Updated deleteTask function

--- src/App.vue
+++ src/App.vue
@@ -27,9 +27,13 @@ export default {
     }
   },
   methods: {
-    deleteTask(id) {
+    async deleteTask(id) {
       if (confirm('Are you sure?')) {
-        this.tasks = this.tasks.filter((task) => task.id !== id) 
+        const res = await fetch(`api/tasks/${id}`, {
+          method: 'DELETE',
+        })
+
+        res.status === 200 ? (this.tasks = this.tasks.filter((task) => task.id !== id)) : alert('Error Deleteing Task')        
       }
     },
     toggleReminder(reminder, id) {
